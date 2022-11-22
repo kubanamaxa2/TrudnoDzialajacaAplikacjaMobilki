c888d549f992f7b56ce8cbe0487511728a540115
Dominos0321 <dsch792@gmail.com>
2022-11-22 09:24:18 +0100

    Zrobiony Layout

--- app/src/main/java/com/mhb/trudnodzialajacaaplikacjamobilki/MainActivity.kt
+++ app/src/main/java/com/mhb/trudnodzialajacaaplikacjamobilki/MainActivity.kt
@@ -14,5 +14,14 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        var s1 = binding.s1
+        var s2 = binding.s2
+        var s3 = binding.s3
+        var s4 = binding.s4
+        var s5 = binding.s5
+        var s6 = binding.s6
+        var s7 = binding.s7
+        var s8 = binding.s8
+        var s9 = binding.s9
     }
 }
